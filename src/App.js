7ab0f16ef213cd1f3954d21aceb190caa50082ef
Patrick Collins <patrickncollins@gmail.com>
2017-01-27 01:29:16 -0500

    Added some Bootstrap navbars

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import React, { Component } from 'react';
+import {Grid, Navbar, Nav, NavItem, Row, Col, Jumbotron, Button} from 'react-bootstrap';
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div>
+            <Navbar inverse staticTop>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        ERP
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav pullRight>
+                        <NavItem>Sign In</NavItem>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            <Navbar staticTop>
+                <Navbar.Toggle />
+                <Navbar.Collapse>
+                    <Nav bsStyle="tabs" justified>
+                        <NavItem>Home</NavItem>
+                        <NavItem>Setup</NavItem>
+                        <NavItem>Upload Documents</NavItem>
+                        <NavItem>Rebate Estimate</NavItem>
+                        <NavItem>Rebate Confirmation</NavItem>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
     );
   }
 }
